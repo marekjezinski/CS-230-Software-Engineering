a54427a6d73b22987e6a81642ef363cea922b852
Caleb Ocansey <cocansey11@gmail.com>
2022-11-30 17:01:05 +0000

    Update Player.java
    constructor for a Player

--- cs230/src/main/java/CS230/Player.java
+++ cs230/src/main/java/CS230/Player.java
@@ -3,5 +3,11 @@ package CS230;
 import javafx.scene.image.Image;
 
 public class Player extends Character {
-    
+
+    //constructor for Player
+    public Player(int x, int y, Image playerPic) {
+        super(x,y,playerPic);
+    }
+
+
 }
