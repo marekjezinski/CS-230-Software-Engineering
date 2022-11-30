764ac125bfb4093f1cb804482776c38bbba04f6d
Caleb Ocansey <cocansey11@gmail.com>
2022-11-30 16:36:43 +0000

    Update Character.java
    attributes for character

--- cs230/src/main/java/CS230/Character.java
+++ cs230/src/main/java/CS230/Character.java
@@ -1,5 +1,13 @@
 package CS230;
 
+import javafx.scene.image.Image;
 
 public class Character {
+    private int xPos;
+    private int yPos;
+    private String CharType;
+    private boolean alive;
+    private Image charImage;
+
+
 }
