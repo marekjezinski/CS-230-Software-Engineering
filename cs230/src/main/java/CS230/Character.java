2d62bd1ce0a2db5334d3cea65b24f3650031ecb5
Caleb Ocansey <cocansey11@gmail.com>
2022-11-30 16:39:35 +0000

    Update Character.java
    getters and setters for character

--- cs230/src/main/java/CS230/Character.java
+++ cs230/src/main/java/CS230/Character.java
@@ -9,5 +9,26 @@ public class Character {
     private boolean alive;
     private Image charImage;
 
+    public int getYPos() {
+        return yPos;
+    }
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public String getCharType() {
+        return CharType;
+    }
+
+    public Image getCharImage() {
+        return charImage;
+    }
+
+    // public void moveChar() to be implemented...
 
 }
