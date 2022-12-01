fb20c2ddbdc3e1126aeabdb7f2e8a5ca14447805
alexcarmen21 <alexcarmen@hotmail.es>
2022-12-01 16:36:16 +0000

    these are the Gate and Lever classes. haven't implemented them into MapReader.java, Map.java or Main.java yet

---
+++ cs230/src/main/java/CS230/items/Gate.java
@@ -0,0 +1,11 @@
+package CS230.items;
+import java.util.ArrayList;
+
+import CS230.MapReader;
+import javafx.scene.image.Image;
+
+public class Gate extends Item {
+    public Gate(Image img, int x, int y) {
+        super(img, x, y);
+    }
+}
---
+++ cs230/src/main/java/CS230/items/Lever.java
@@ -0,0 +1,11 @@
+package CS230.items;
+import java.util.ArrayList;
+
+import CS230.MapReader;
+import javafx.scene.image.Image;
+
+public class Lever extends Item {
+    public Lever(Image img, int x, int y) {
+        super(img, x, y);
+    }
+}
