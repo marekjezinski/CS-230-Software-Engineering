4a107ac02144265214e33aaa01d30067b3f48353
Jerseystudent <90096181+Jerseystudent@users.noreply.github.com>
2022-12-01 14:57:49 +0000

    limits output of leaderboard to 10

--- cs230/src/main/java/CS230/Leaderboard.java
+++ cs230/src/main/java/CS230/Leaderboard.java
@@ -4,15 +4,18 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Leaderboard extends SaveLoad {
+    private ArrayList<String> names = new ArrayList<String>();
+    private ArrayList<Integer> scores = new ArrayList<Integer>();
 
     public void addScore (String username, int score) {
         try{
-            FileWriter f = new FileWriter("scores.txt", true);
-            f.write(username + " " + score + System.lineSeparator());
-            f.close();
+            FileWriter fI = new FileWriter("scores.txt", true);
+            fI.write(System.lineSeparator() + username +" "+ score);
+            fI.close();
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -21,25 +24,46 @@ public class Leaderboard extends SaveLoad {
 
     }
 
+
     private void addUser(){
         String username = null;
         //collect inputs in gui
         //upload to txt file
     }
-
-    public String getTopScores(){
+    public String getAllScores(){
         String scores = "";
         File f = new File("scores.txt");
         try  {
             Scanner in = new Scanner(f);
             while(in.hasNextLine()){
-                System.out.println(in.nextLine());
+                scores = scores + in.nextLine();
             }
         }
         catch (FileNotFoundException exception) {
             exception.printStackTrace();
         }
         return scores;
+    }
+
+    public void getTopScores(){
+        int topScore = 0;
+        File f = new File("scores.txt");
+        try  {
+            Scanner in = new Scanner(f);
+            while(in.hasNext()) {
+                this.names.add(in.next());
+
+                this.scores.add(in.nextInt());
+            }
+           for (int i = 0; i < 10; i++){
+               System.out.println(this.names.get(i) + " " + this.scores.get(i));
+           }
+                }
+
+
+        catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        }
     }
 
     private Object showTop10Scores(){
