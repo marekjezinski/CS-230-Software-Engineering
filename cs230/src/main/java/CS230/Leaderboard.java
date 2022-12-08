3d7e487bba50b5b187ebb71473cc6a23bde68472
Jerseystudent <90096181+Jerseystudent@users.noreply.github.com>
2022-12-08 16:25:02 +0000

    Only does scores for one player
    Avoids player getting on board multiple times

--- cs230/src/main/java/CS230/Leaderboard.java
+++ cs230/src/main/java/CS230/Leaderboard.java
@@ -46,6 +46,20 @@ public class Leaderboard extends SaveLoad {
                 this.names.add(in.next());
                 this.scores.add(in.nextInt());
             }
+            for(int i = 0; i < this.names.size(); i++){
+                for(int j = 1; j < this.names.size(); j++){
+                    if (this.names.get(i) == this.names.get(j)){
+                        if (this.scores.get(i) > this.scores.get(j)){
+                            this.scores.remove(j);
+                            this.names.remove(j);
+                        }
+                        else {
+                            this.scores.remove(i);
+                            this.names.remove(i);
+                        }
+                    }
+                }
+            }
            for (int i = 0; i < 10; i++) {
                if (this.scores.size() > 0) {
                    int topScore = 0;
