a74ea1350d5c1bbceb36aa67fa4392ed6cbad7d6
Caleb Ocansey <cocansey11@gmail.com>
2022-12-08 10:38:37 +0000

    Creating SmartThief
    still deciding on how to implement BFS - as thats how it decides what movements. to make

---
+++ cs230/src/main/java/CS230/npc/SmartThief.java
@@ -0,0 +1,16 @@
+package CS230.npc;
+
+import CS230.Character;
+import javafx.scene.image.Image;
+
+public class SmartThief extends Character {
+
+    //private currentPath --  a current path for the npc to move on
+    //constructor for smartThief
+    public SmartThief (int x, int y, Image smThiefPic) {
+        super(x,y,smThiefPic);
+    }
+
+    //to be implemented after smartthief map BFS
+    //public void moveSmartThief(){}
+}
