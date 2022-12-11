2083bdda0091aeb59f2b1e50ab08fe0bd64777e7
alexcarmen21 <alexcarmen@hotmail.es>
2022-12-11 15:38:37 +0000

    javadoc comments added to this class

--- cs230/src/main/java/CS230/saveload/PlayerProfile.java
+++ cs230/src/main/java/CS230/saveload/PlayerProfile.java
@@ -12,30 +12,58 @@ public class PlayerProfile {
     private int score;
     private int maxLevel;
 
+    /**
+     * Constructor that creates a Player Profile
+     * @param username - the user's unique name
+     */
     public PlayerProfile(String username){
             this.username = username;
     }
 
+    /**
+     * method that sets the Player's max level
+     * @param maxLevel
+     */
     public void setMaxLevel(int maxLevel) {
         this.maxLevel = maxLevel;
     }
 
+    /**
+     * method that sets the Player's score
+     * @param score
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * method that sets the Player's unique username
+     * @param username
+     */
     public void setUsername(String username){
         this.username = username;
     }
 
+    /**
+     * method that gets the max score
+     * @return score
+     */
     public int getMaxScore(){
             return this.score;
         }
 
+    /**
+     * method that gets the max level
+      * @return maxLevel
+     */
     public int getMaxLevel(){
         return this.maxLevel;
     }
 
+    /**
+     * method that gets the Player's username
+     * @return username
+     */
     public String getUsername() {
             return username;
         }
